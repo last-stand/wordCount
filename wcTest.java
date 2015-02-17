609701531fb82d9e501d0c2a7a180a6287023f1c
Jai Prakash <jaiprak@thoughtworks.com>
2015-02-17 14:30:37 +0530

    Initial commit

---
+++ wcTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+class WCTest {
+
+    @Test
+    public void thisAlwaysPasses() {
+
+    }
+
+    @Test
+    public void thisIsIgnored() {
+    }
+}
