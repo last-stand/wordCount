820ce3f4b56301384d927e975a3a62628306f2c3
Jai Prakash <jaiprak@thoughtworks.com>
2015-02-18 10:39:23 +0530

    WC Version 1 < counting words from string is completed. / new tests are added.

---
+++ WordCount.java
@@ -0,0 +1,11 @@
+public class WordCount{
+	private String str;
+	public WordCount(String str){
+		this.str = str;
+	}
+
+	public int countWords(){
+		String[] words = str.split("\\s+");
+		return words.length;
+	}
+}
--- wcTest.java
+++ wcTest.java
@@ -1,14 +1,56 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-class WCTest {
+public class WCTest {
 
     @Test
-    public void thisAlwaysPasses() {
-
+    public void countWords_should_count_number_of_words_in_given_string_excluding_spaces() {
+    	String str = "hello, this is a good day.";
+    	WordCount wc = new WordCount(str);
+    	assertSame("should be same", 6, wc.countWords());
     }
 
     @Test
-    public void thisIsIgnored() {
+    public void countWords_should_count_number_of_words_in_given_string_excluding_tabs() {
+    	String str = "hello,	this is a good	day.";
+    	WordCount wc = new WordCount(str);
+    	assertSame("should be same", 6, wc.countWords());
+    }
+
+    @Test
+    public void countWords_should_count_number_of_words_in_given_string_excluding_tabs_and_spaces() {
+    	String str = "hello,	this 	is a 	good	day.";
+    	WordCount wc = new WordCount(str);
+    	assertSame("should be same", 6, wc.countWords());
+    }
+
+    @Test
+    public void countWords_should_count_number_of_words_in_given_string_excluding_newLines() {
+    	String str = "Twinkle, twinkle, little star"+
+			    	 "\nHow I wonder what you are"+
+			    	 "\nUp above the world so high"+
+			    	 "\nLike a diamond in the sky";
+    	WordCount wc = new WordCount(str);
+    	assertSame("should be same", 22, wc.countWords());
+    }
+
+    @Test
+    public void countWords_should_count_number_of_words_in_given_string_excluding_carriage_return(){
+    	String str = "Jack and Jill went up the hill"+
+			    	 "\r\nTo fetch a pail of water."+
+			    	 "\r\nJack fell down and broke his crown,"+
+			    	 "\r\nAnd Jill came tumbling after";
+    	WordCount wc = new WordCount(str);
+    	assertSame("should be same", 25, wc.countWords());
+    }
+
+    @Test
+    public void countWords_should_count_number_of_words_in_given_string_excluding_allWhiteSpaces_and_tabs() {
+    	String str = "Twinkle,	twinkle, 	little star"+
+			    	 "\r\nHow I wonder what you are"+
+			    	 "\nUp above the	world 	so high"+
+			    	 "\nLike a diamond  in the sky";
+    	WordCount wc = new WordCount(str);
+    	assertSame("should be same", 22, wc.countWords());
     }
 }
